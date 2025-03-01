5c26baea3a13cffc48dfc2fb4cd397e39724e585
Aryt3 <Jxkobus@protonmail.com>
2025-03-01 17:32:22 +0100

    Adding default media-query-sizes in tailwind-config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -42,6 +42,14 @@ module.exports = {
           "card-muted": withOpacity("--color-card-muted"),
         },
       },
+      screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
       outlineColor: {
         skin: {
           fill: withOpacity("--color-accent"),
